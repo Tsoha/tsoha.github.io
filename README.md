e73cd5420c2b3cb42c910fad29c1e61ab7f8e18a
TeemuKoivisto <teemu.koivisto@helsinki.fi>
2016-05-21 02:01:11 +0300

    Poistettu aikataulu, lisätty express

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Tietokantasovellus-kurssin PHP-materiaali
+
+Jos haluat muokata tätä materiaalia sillei fiksusti niin joudut asentamaan Noden ja npm:n. Sitten lataat tämän kansion, menet juureen ja kirjoitat `npm install`. Sen jälkeen voit käynnistää serverin kirjoittamalla `npm start` ja tekemäsi muutosten pitäisi heti näkyä localhost:8080 osoitteessa.
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + "/"));
+
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("App listening on port", port);
+  }
+});
